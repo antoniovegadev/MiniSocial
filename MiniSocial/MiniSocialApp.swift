f185f5e76c2a7770907d8bade202f5d4c3166b6b
antonivegaochoa <tonivegaochoa@gmail.com>
2021-05-29 00:15:40 -0400

    Initial Commit

---
+++ MiniSocial/MiniSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSocialApp.swift
+//  MiniSocial
+//
+//  Created by Antonio Vega on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MiniSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
